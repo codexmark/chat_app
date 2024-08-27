17770b1688d401c33a74de5052bce3bcda6e9635
codexmark <codexmark@gmail.com>
2024-08-27 13:15:56 -0400

    criando o canal no cable e configurando

---
+++ app/channels/conversation_channel.rb
@@ -0,0 +1,24 @@
+class ConversationChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
+# app/channels/conversation_channel.rb
+class ConversationChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "conversation_#{params[:id]}"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    message = Message.create(conversation_id: params[:id], user_id: current_user.id, content: data['message'])
+    ActionCable.server.broadcast("conversation_#{params[:id]}", message: message.content, user: current_user.username)
+  end
+end
