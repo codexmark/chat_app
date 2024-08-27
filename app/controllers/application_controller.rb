94f9789470528a79be51615963c4f1db47bd35d6
codexmark <codexmark@gmail.com>
2024-08-27 13:19:32 -0400

    criando usuario descartavel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-end
+    before_action :set_current_user
+  
+    private
+  
+    def set_current_user
+      @current_user = User.find_or_create_by(username: SecureRandom.alphanumeric(8))
+    end
+  
+    def current_user
+      @current_user
+    end
+  
+    helper_method :current_user
+  end
