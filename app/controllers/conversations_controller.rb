58ed5380e156f07d357c81ea70596b13b291ad8e
codexmark <codexmark@gmail.com>
2024-08-27 13:23:31 -0400

    bug fix

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,8 @@
+
 # app/controllers/conversations_controller.rb
 class ConversationsController < ApplicationController
   def create
-    conversation = Conversation.create(id: SecureRandom.alphanumeric(8), participants: 0)
+    conversation = Conversation.create(participants: 0)
     render json: { id: conversation.id }
   end
 
@@ -14,5 +15,10 @@ class ConversationsController < ApplicationController
     conversation = Conversation.find_by(id: params[:id])
     conversation.destroy if conversation && conversation.created_at < 8.hours.ago
     head :no_content
+  end
+
+  def index
+    @conversations = Conversation.all
+    render json: @conversations
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/conversations/:id', to: 'conversations#show'
   delete '/conversations/:id', to: 'conversations#destroy'
   post '/conversations/:id/messages', to: 'messages#create'
+  get '/conversations', to: 'conversations#index' # Adicione essa linha
 end
