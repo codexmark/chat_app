6bdb06e2facbd2dfb1d1d9a7b77e298d5ed57c7e
codexmark <codexmark@gmail.com>
2024-08-27 14:54:37 -0400

    arrumando a view index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,18 @@
+# app/controllers/welcome_controller.rb
 class WelcomeController < ApplicationController
   def index
   end
 
   def create
+    @user = User.find_or_create_by(username: params[:username])
+    session[:user_id] = @user.id
+    @conversation = Conversation.find_by(id: params[:conversation_id])
+    if @conversation
+      session[:conversation_id] = @conversation.id
+      redirect_to conversation_path(@conversation.id)
+    else
+      flash[:error] = "Conversa não encontrada"
+      render :index
+    end
   end
 end
