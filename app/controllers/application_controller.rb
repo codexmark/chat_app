9cf6911328941b2cc64f70f294259ca1f981aa37
codexmark <codexmark@gmail.com>
2024-08-27 14:43:49 -0400

    configurando o helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,11 @@ class ApplicationController < ActionController::Base
       @current_user
     end
   
-    helper_method :current_user
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
