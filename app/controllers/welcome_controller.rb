8d99f29be359509dae9aabb47ba62dcbd5f16db1
codexmark <codexmark@gmail.com>
2024-08-27 15:11:53 -0400

    modificando o controller para caso nao exista id da conversa, ele crie uma nova

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,13 +6,17 @@ class WelcomeController < ApplicationController
   def create
     @user = User.find_or_create_by(username: params[:username])
     session[:user_id] = @user.id
-    @conversation = Conversation.find_by(id: params[:conversation_id])
-    if @conversation
-      session[:conversation_id] = @conversation.id
-      redirect_to conversation_path(@conversation.id)
-    else
-      flash[:error] = "Conversa não encontrada"
-      render :index
+
+    # Buscar conversa pelo ID
+    @conversation = Conversation.find_or_create_by(id: params[:conversation_id])
+
+    # Se a conversa for nova, inicializar os participantes
+    if @conversation.new_record?
+      @conversation.participants = 0
+      @conversation.save
     end
+
+    session[:conversation_id] = @conversation.id
+    redirect_to conversation_path(@conversation.id)
   end
 end
