f84d95dd022db139a6784b88be01a6dbf332bdf7
codexmark <codexmark@gmail.com>
2024-08-27 14:37:51 -0400

    configurando sessions e uma view para usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+# config/routes.rb
 Rails.application.routes.draw do
   post '/conversations', to: 'conversations#create'
   get '/conversations/:id', to: 'conversations#show'
   delete '/conversations/:id', to: 'conversations#destroy'
   post '/conversations/:id/messages', to: 'messages#create'
-  get '/conversations', to: 'conversations#index' # Adicione essa linha
+  get '/conversations', to: 'conversations#index'
 end
