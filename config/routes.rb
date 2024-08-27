0f81e2ab9e903539025b1ee073dc3b704873970f
codexmark <codexmark@gmail.com>
2024-08-27 13:10:03 -0400

    criando controladores e rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/create'
+  get 'conversations/create'
+  get 'conversations/show'
+  get 'conversations/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
