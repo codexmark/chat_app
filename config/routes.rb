dd1f248b001c0e2d17aaf8098a7b78fa2da9afe5
codexmark <codexmark@gmail.com>
2024-08-27 14:56:49 -0400

    ajustando as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/create'
   post '/conversations', to: 'conversations#create'
   get '/conversations/:id', to: 'conversations#show', as: :conversation
   delete '/conversations/:id', to: 'conversations#destroy'
   post '/conversations/:id/messages', to: 'messages#create'
   get '/conversations', to: 'conversations#index'
-  get '/sessions/new', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create'
+  get '/welcome', to: 'welcome#index'
+  post '/welcome', to: 'welcome#create'
+  root to: 'welcome#index'
 end
