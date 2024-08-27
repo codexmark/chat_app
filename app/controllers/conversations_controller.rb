33b4f0470736a00c0ee39629862ab345440f954d
codexmark <codexmark@gmail.com>
2024-08-27 13:14:05 -0400

    implementadno a logica nas conversas e nas mensagens

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,10 +1,18 @@
+# app/controllers/conversations_controller.rb
 class ConversationsController < ApplicationController
   def create
+    conversation = Conversation.create(id: SecureRandom.alphanumeric(8), participants: 0)
+    render json: { id: conversation.id }
   end
 
   def show
+    @conversation = Conversation.find_by(id: params[:id])
+    render json: @conversation
   end
 
   def destroy
+    conversation = Conversation.find_by(id: params[:id])
+    conversation.destroy if conversation && conversation.created_at < 8.hours.ago
+    head :no_content
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,8 @@
+# app/controllers/messages_controller.rb
 class MessagesController < ApplicationController
   def create
+    conversation = Conversation.find_by(id: params[:id])
+    message = Message.create(conversation_id: conversation.id, user_id: current_user.id, content: params[:content])
+    render json: message
   end
 end
